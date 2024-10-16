179d0a149a50e005eacc79cd24284da5da2623bf
Jumaaly Saaparaliev <saparalievjuaamaly@gmail.com>
2024-10-17 01:02:12 +0600

    add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return <div className="container">
+     <p className="text">Favorite Movies</p>
+
+     <button className="addButton">ADD MOVIE</button>
+  </div>;
+}
